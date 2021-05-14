a47da56c2f4b9ef5ac9709b5083d8a7a8c835f90
Bartłomiej Kalka <bartlomiejkalqa@gmail.com>
2021-05-14 12:15:20 +0100

    Added axon configuration annotations for aggregates

--- src/main/java/com/cinemafranchise/domain/movie/Movie.java
+++ src/main/java/com/cinemafranchise/domain/movie/Movie.java
@@ -1,11 +1,18 @@
 package com.cinemafranchise.domain.movie;
 
 import com.cinemafranchise.shared.common.MovieId;
+import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.spring.stereotype.Aggregate;
 
+@Aggregate
 public class Movie {
 
+    @AggregateIdentifier
     MovieId movieId;
     MovieImdbId movieImdbId;
     MovieTitle movieTitle;
     MovieRating movieRating;
+
+    protected Movie() {
+    }
 }
--- src/main/java/com/cinemafranchise/domain/movieshow/MovieShow.java
+++ src/main/java/com/cinemafranchise/domain/movieshow/MovieShow.java
@@ -1,11 +1,18 @@
 package com.cinemafranchise.domain.movieshow;
 
 import com.cinemafranchise.shared.common.MovieId;
+import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.spring.stereotype.Aggregate;
 
+@Aggregate
 public class MovieShow {
 
+    @AggregateIdentifier
     MovieShowId movieShowId;
     MovieId movieId;
     Price price;
     ShowTime showTime;
+
+    protected MovieShow() {
+    }
 }
