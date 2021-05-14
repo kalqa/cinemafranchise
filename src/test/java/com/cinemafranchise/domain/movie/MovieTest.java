956e59115567590d8e660b18ffca03b1e36acf01
Bartłomiej Kalka <bartlomiejkalqa@gmail.com>
2021-05-14 12:02:33 +0100

    Added test cases for events and commands

---
+++ src/test/java/com/cinemafranchise/domain/movie/MovieTest.java
@@ -0,0 +1,27 @@
+package com.cinemafranchise.domain.movie;
+
+import org.junit.jupiter.api.Test;
+
+public class MovieTest {
+
+    @Test
+    public void should_expect_movie_created_event_when_create_movie_command_was_passed(){
+
+    }
+
+    @Test
+    public void should_not_expect_movie_created_event_when_create_movie_command_was_not_passed(){
+
+    }
+
+    @Test
+    public void should_expect_movie_rated_event_when_rate_movie_command_was_passed(){
+
+    }
+
+    @Test
+    public void should_not_expect_movie_rated_event_when_rate_movie_command_was_not_passed(){
+
+    }
+
+}
---
+++ src/test/java/com/cinemafranchise/domain/movieshow/MovieShowTest.java
@@ -0,0 +1,26 @@
+package com.cinemafranchise.domain.movieshow;
+
+import org.junit.jupiter.api.Test;
+
+public class MovieShowTest {
+
+    @Test
+    public void should_expect_movie_show_price_changed_event_when_change_movie_show_price_command_was_passed(){
+
+    }
+
+    @Test
+    public void should_not_expect_movie_show_price_changed_event_when_change_movie_show_price_command_was_not_passed(){
+
+    }
+
+    @Test
+    public void should_expect_movie_show_time_changed_event_when_change_movie_show_time_command_was_passed(){
+
+    }
+
+    @Test
+    public void should_not_expect_movie_show_time_changed_event_when_change_movie_show_time_command_was_not_passed(){
+
+    }
+}
